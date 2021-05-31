c5417918b901998a5c0ee92749615155442a1cef
RedFoxito <79026117+RedFoxito@users.noreply.github.com>
2021-05-31 14:50:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-//scrol up
+/// Scroll up
+document.getElementById("button-up").addEventListener("click", scrollUp);
 
-document.getElementById("button-up").addEventListener("click",scrollup);
-
-function scrollup(){
+function scrollUp(){
 
     var currentScroll = document.documentElement.scrollTop;
 
     if (currentScroll > 0){
-        window.scrollTo(0,0);
+        window.scrollTo (0, 0);
     }
 }
 
-buttonUp= document.getElementById("button-up");
+///
+buttonUp = document.getElementById("button-up");
 
-window.onScroll = function(){
-    var scroll= document.documentElement.scrollTop;
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
 
     if (scroll > 500){
         buttonUp.style.transform = "scale(1)";
-    }else if (sroll < 500){
-        buttonup.style.transform="scale(0)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
     }
+
 }
